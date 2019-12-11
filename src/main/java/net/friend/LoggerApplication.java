fa1ba3b6ef3955ff3c9f03663dba5fb767926730
quanbh2 <quanbh2@topica.edu.vn>
2019-12-11 10:02:21 +0700

    init project

---
+++ src/main/java/net/friend/LoggerApplication.java
@@ -0,0 +1,18 @@
+package net.friend;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class LoggerApplication implements CommandLineRunner {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoggerApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {}
+}
